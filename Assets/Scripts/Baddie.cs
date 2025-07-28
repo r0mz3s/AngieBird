4846ace422fea1922be4838680644e5917b409e0
r0mz3s <qa.romann@gmail.com>
2025-07-28 21:11:39 +0300

    second commit

--- Assets/Scripts/Baddie.cs
+++ Assets/Scripts/Baddie.cs
@@ -2,5 +2,35 @@ using UnityEngine;
 
 public class Baddie : MonoBehaviour
 {
-  
+    [SerializeField] private float maxHealth = 3f;
+    [SerializeField] private float damageThreshold = 0.2f;
+    private float currentHealth;
+
+    private void Awake()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void DamageBaddi(float damageAmount)
+    { 
+        currentHealth -= damageAmount;
+        if (currentHealth <= 0f)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        float impactVelocity = collision.relativeVelocity.magnitude;
+        if (impactVelocity > damageThreshold)
+        {
+            DamageBaddi(impactVelocity);
+        }
+    }
 }
